88946a67bac4894c564427b726ede2f7ee6925dd
Jonathan Reátegui <j.reategui2012@gmail.com>
2021-10-18 00:32:09 -0500

    Adding second text view for characteristic feature

--- app/src/main/java/com/example/lab02_taxonomia_generaciones/MainActivity.kt
+++ app/src/main/java/com/example/lab02_taxonomia_generaciones/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lab02_taxonomia_generaciones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnVerificationButton.setOnClickListener {
+
+            var yearOfBirth = edtYearOfBirth.text.toString().toInt()
+
+            tvPopulationNumber.text = when(yearOfBirth) {
+                in 1969 .. 1980 -> {
+                    "GENERACIÓN X"
+                }
+                in 1981 .. 1993 -> {
+                    "GENERACIÓN Y"
+                }
+                in 1994 .. 2010 -> {
+                    "GENERACIÓN Z"
+                } else -> {
+                    "No perteneces a ninguna generación"
+                }
+            }
+
+        }
+
     }
 }
